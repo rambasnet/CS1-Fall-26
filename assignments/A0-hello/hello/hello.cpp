22e964d494e9291236cdb1198b01b7e0c389fac8
Ram Basnet <rambasnet@gmail.com>
2026-08-26 17:19:37 +0000

    complete assignment 0

---
+++ assignments/A0-hello/hello/hello.cpp
@@ -0,0 +1,18 @@
+/*
+Hello World Program
+Programmer: Ram Basnet
+Date: Aug. 24, 2026
+
+Algorithm Steps:
+    1. Output "Hello World!" on the console/standard output
+*/
+
+#include <iostream>
+using namespace std;
+
+// entry function; must have one main
+int main() { // main function
+    cout << "Hello World!" << std::endl;
+    // must return 0; telling the OS that this program was successful!
+    return 0;
+}
